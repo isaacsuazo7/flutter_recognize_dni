ad3babacb9a31aa3cc33811b2f59d6d3f21660a6
Isaac Suazo <isaac.suazo@cit.hn>
2024-12-06 21:38:47 -0600

    03 - refactor: global

--- lib/src/home_screen.dart
+++ lib/src/home_screen.dart
@@ -9,76 +9,78 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('DNI Recognizer'),
+        title: const Text('Reconocimiento de DNI'),
       ),
       body: Center(
         child: ElevatedButton(
-          onPressed: () async {
-            final ImagePicker picker = ImagePicker();
-            final XFile? image =
-                await picker.pickImage(source: ImageSource.gallery);
-
-            if (image == null) {
-              if (!context.mounted) return;
-              ScaffoldMessenger.of(context).showSnackBar(
-                const SnackBar(
-                  content: Text('No se selecciono ninguna imagen'),
-                ),
-              );
-              return;
-            }
-
-            final textRecognizer =
-                TextRecognizer(script: TextRecognitionScript.latin);
-            final recognized = await textRecognizer
-                .processImage(InputImage.fromFilePath(image.path));
-
-            if (!isDNI(recognized.text)) {
-              if (!context.mounted) return;
-              ScaffoldMessenger.of(context).showSnackBar(
-                const SnackBar(
-                  content: Text('NO ES UN DNI'),
-                ),
-              );
-              return;
-            }
-
-            Map<String, String> extractedData = {};
-            final data = recognized.text.split('\n');
-
-            for (var i = 0; i < data.length; i++) {
-              String key = findKey(data[i]);
-              if (key.isNotEmpty && i + 1 < data.length) {
-                extractedData[key] = data[i + 1];
-              }
-            }
-
-            await textRecognizer.close();
-
-            if (!context.mounted) return;
-
-            showDialog(
-              context: context,
-              builder: (context) {
-                return AlertDialog(
-                  title: const Text('Datos extraidos'),
-                  content: SingleChildScrollView(
-                    child: ListBody(
-                      children: extractedData.entries
-                          .map((e) => ListTile(
-                                title: Text(e.key),
-                                subtitle: Text(e.value),
-                              ))
-                          .toList(),
-                    ),
-                  ),
-                );
-              },
-            );
+          onPressed: () {
+            scanDNI(context);
           },
           child: const Text('Abrir galeria'),
         ),
       ),
+    );
+  }
+
+  void scanDNI(BuildContext context) async {
+    final ImagePicker picker = ImagePicker();
+    final XFile? image = await picker.pickImage(source: ImageSource.gallery);
+
+    if (image == null) {
+      if (!context.mounted) return;
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(
+          content: Text('No se selecciono ninguna imagen'),
+        ),
+      );
+      return;
+    }
+
+    final textRecognizer = TextRecognizer(script: TextRecognitionScript.latin);
+    final recognized =
+        await textRecognizer.processImage(InputImage.fromFilePath(image.path));
+
+    if (!isDNI(recognized.text)) {
+      if (!context.mounted) return;
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(
+          content: Text('NO ES UN DNI'),
+        ),
+      );
+      return;
+    }
+
+    Map<String, String> extractedData = {};
+    final data = recognized.text.split('\n');
+
+    for (var i = 0; i < data.length; i++) {
+      String key = findKey(data[i]);
+      if (key.isNotEmpty && i + 1 < data.length) {
+        extractedData[key] = data[i + 1];
+      }
+    }
+
+    await textRecognizer.close();
+
+    if (!context.mounted) return;
+
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: const Text('Datos extraidos'),
+          content: SingleChildScrollView(
+            child: ListBody(
+              children: extractedData.entries
+                  .map((e) => ListTile(
+                        title: Text(e.key),
+                        subtitle: Text(e.value),
+                      ))
+                  .toList(),
+            ),
+          ),
+        );
+      },
     );
   }
 
