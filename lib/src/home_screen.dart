d21f8bc5e8fc0a6f37fba625ca810829177b36c6
Isaac Suazo <isaac.suazo@cit.hn>
2024-12-06 21:03:13 -0600

    02- feat: funcionamiento basico

--- lib/src/home_screen.dart
+++ lib/src/home_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:google_mlkit_text_recognition/google_mlkit_text_recognition.dart';
+import 'package:image_picker/image_picker.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -11,10 +13,114 @@ class HomeScreen extends StatelessWidget {
       ),
       body: Center(
         child: ElevatedButton(
-          onPressed: () async {},
-          child: const Text('Abrir camara'),
+          onPressed: () async {
+            final ImagePicker picker = ImagePicker();
+            final XFile? image =
+                await picker.pickImage(source: ImageSource.gallery);
+
+            if (image == null) {
+              if (!context.mounted) return;
+              ScaffoldMessenger.of(context).showSnackBar(
+                const SnackBar(
+                  content: Text('No se selecciono ninguna imagen'),
+                ),
+              );
+              return;
+            }
+
+            final textRecognizer =
+                TextRecognizer(script: TextRecognitionScript.latin);
+            final recognized = await textRecognizer
+                .processImage(InputImage.fromFilePath(image.path));
+
+            if (!isDNI(recognized.text)) {
+              if (!context.mounted) return;
+              ScaffoldMessenger.of(context).showSnackBar(
+                const SnackBar(
+                  content: Text('NO ES UN DNI'),
+                ),
+              );
+              return;
+            }
+
+            Map<String, String> extractedData = {};
+            final data = recognized.text.split('\n');
+
+            for (var i = 0; i < data.length; i++) {
+              String key = findKey(data[i]);
+              if (key.isNotEmpty && i + 1 < data.length) {
+                extractedData[key] = data[i + 1];
+              }
+            }
+
+            await textRecognizer.close();
+
+            if (!context.mounted) return;
+
+            showDialog(
+              context: context,
+              builder: (context) {
+                return AlertDialog(
+                  title: const Text('Datos extraidos'),
+                  content: SingleChildScrollView(
+                    child: ListBody(
+                      children: extractedData.entries
+                          .map((e) => ListTile(
+                                title: Text(e.key),
+                                subtitle: Text(e.value),
+                              ))
+                          .toList(),
+                    ),
+                  ),
+                );
+              },
+            );
+          },
+          child: const Text('Abrir galeria'),
         ),
       ),
     );
+  }
+
+  bool isDNI(String text) {
+    text = text.toUpperCase();
+    final isDNI = text.contains('REGISTRO NACIONAL') &&
+        text.contains('REPÚBLICA DE HONDURAS');
+
+    return isDNI;
+  }
+
+  String findKey(String line) {
+    line = line.toLowerCase();
+    if (line.contains('nombre')) {
+      return 'Nombre';
+    }
+
+    if (line.contains('apellido') || line.contains('surname')) {
+      return 'Apellido';
+    }
+
+    if (line.contains('fecha de nacimiento') ||
+        line.contains('date of birth')) {
+      return 'Fecha de nacimiento';
+    }
+
+    if (line.contains('nacionalidad') || line.contains('nationality')) {
+      return 'Nacionalidad';
+    }
+
+    if (line.contains('lugar') || line.contains('place')) {
+      return 'Lugar de Nacimiento';
+    }
+
+    if (line.contains('id number')) {
+      return 'Número de Identidad';
+    }
+
+    if (line.contains('expiración') || line.contains('expiry')) {
+      return 'Fecha de Expiración';
+    }
+
+    return '';
   }
 }
